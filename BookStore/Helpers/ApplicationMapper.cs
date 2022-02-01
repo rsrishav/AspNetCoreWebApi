3af19687febd7dff34b3d065202bcbf4408a603d
Rishav Sharma <rsrishav0@gmail.com>
2022-02-01 21:24:22 +0530

    Auto mapper functionality added using 'AutoMapper.Extensions.Microsoft.DependencyInjection'.

---
+++ BookStore/Helpers/ApplicationMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BookStore.Data;
+using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Helpers
+{
+    public class ApplicationMapper : Profile
+    {
+        public ApplicationMapper()
+        {
+            CreateMap<Books, BookModel>().ReverseMap();
+        }
+    }
+}
--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -33,6 +33,7 @@ namespace BookStore
             });
 
             services.AddTransient<IBookRepository, BookRepository>();
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
