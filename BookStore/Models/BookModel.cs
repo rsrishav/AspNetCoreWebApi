83d66b8769aeb854f54aa8fd7e9c838bf049d273
Rishav Sharma <rsrishav0@gmail.com>
2022-02-01 21:08:22 +0530

    Model Validation implemented.

--- BookStore/Models/BookModel.cs
+++ BookStore/Models/BookModel.cs
@@ -1,9 +1,17 @@
-﻿namespace BookStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
 {
     public class BookModel
     {
         public int Id { get; set; }
+        
+        //[Required]      // Default message: "The Title field is required."
+        [Required(ErrorMessage = "Please add Title property.")]
         public string Title { get; set; }
+        
+        [MinLength(5)]
+        [MaxLength(20)]
         public string Description { get; set; }
     }
 }
